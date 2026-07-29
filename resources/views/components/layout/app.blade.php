4eda18096f2af421ea193428d4a25663f2d6dfe8
terryeah <terryeah7@gmail.com>
2026-07-29 14:03:57 +1000

    feat(fonts): ship subset woff2 gmarket sans with head preloads for instant korean text rendering
    Also wires the optional Cloudflare Web Analytics beacon into the layout head.

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -14,7 +14,12 @@
     @endif
     <meta property="og:title" content="{{ $title ?? config('app.name') }}">
     <meta property="og:site_name" content="{{ config('app.name') }}">
+    <link rel="preload" href="/fonts/GmarketSansMedium.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="/fonts/GmarketSansBold.woff2" as="font" type="font/woff2" crossorigin>
     @vite(['resources/css/app.css', 'resources/ts/app.ts'])
+    @if (config('services.cloudflare.web_analytics_token'))
+        <script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "{{ config('services.cloudflare.web_analytics_token') }}"}'></script>
+    @endif
 </head>
 <body class="flex min-h-screen flex-col bg-paper font-sans text-navy antialiased">
     <x-layout.header />
