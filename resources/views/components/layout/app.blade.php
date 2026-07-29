46671af7292219c22529eb683fecfe9a74e2c200
terryeah <terryeah7@gmail.com>
2026-07-30 09:24:32 +1000

    fix(layout): put the church name before the page title in the browser tab title format

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $title ? $title.' · ' : '' }}{{ config('app.name') }}</title>
+    <title>{{ config('app.name') }}{{ $title ? ' · '.$title : '' }}</title>
     @if ($description)
         <meta name="description" content="{{ $description }}">
     @endif
