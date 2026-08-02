f3fc5f495f865ac2bc15b50eb9954db810646411
terryeah <terryeah7@gmail.com>
2026-08-02 21:48:02 +1000

    fix(admin): import the schema get utility missing from the member form closures

--- app/Filament/Resources/Members/Schemas/MemberForm.php
+++ app/Filament/Resources/Members/Schemas/MemberForm.php
@@ -13,6 +13,7 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
 use Spatie\Permission\Models\Role;
 
